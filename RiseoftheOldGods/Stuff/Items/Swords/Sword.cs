4e941813e8d3abc8077989b53719de4ea523cead
WingsGivesYouWings <purselmckinley@gmail.com>
2025-06-23 16:26:42 -0400

    Add files via upload

--- RiseoftheOldGods/Stuff/Items/Swords/Sword.cs
+++ RiseoftheOldGods/Stuff/Items/Swords/Sword.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.UI;
+using Terraria.ID;
 
 namespace RiseoftheOldGods.Stuff.Items.Swords
 {
     class Sword
     {
+
     }
 }
