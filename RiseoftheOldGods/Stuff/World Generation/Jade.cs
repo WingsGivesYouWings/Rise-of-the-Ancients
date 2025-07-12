9cac5388e885bd16ee0ce775a89757a90face9fa
Spyder022 <90011564+Spyder022@users.noreply.github.com>
2025-07-12 00:49:49 -0500

    Jade.cs

--- RiseoftheOldGods/Stuff/World Generation/Jade.cs
+++ RiseoftheOldGods/Stuff/World Generation/Jade.cs
@@ -1,8 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.UI;
+using Terraria.ID;
 
 namespace RiseoftheOldGods.Stuff.World_Generation
 {
